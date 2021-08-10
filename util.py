fc2e9147f3835e62d23706f31a2ea5355c8c8704
samanbo <moein.saman@gmail.com>
2021-08-10 15:30:15 +0200

    Defining numpy array class for building validation and train datasets.

--- util.py
+++ util.py
@@ -7,6 +7,35 @@ import torch
 import librosa
 
 
+class numpy_array:
+    def __init__(self, shape):
+        self.shape = shape
+        self.array = np.zeros(shape=self.shape)
+    
+    def transform(self, np_array, count):
+        self.array[count, : 6, :, :] = np_array
+        np_array = np.transpose(np_array, (0, 2, 1))
+        mel = np.array([
+            librosa.feature.melspectrogram(S=x, sr=192000, n_mels=256, fmax=96000) \
+            for x in np_array
+        ])
+        mel = np.transpose(mel, (0, 2, 1))
+        self.array[count, 6:, :, :] = mel
+    
+    def save_dataset(self, label, num, path, name):
+        self.array = torch.from_numpy(self.array).float()
+        label = torch.from_numpy(np.array(label))
+        val_dst = TensorDataset(self.array, label)
+        torch.save(val_dst, '%s/datasets_mel/%s/%s_%d.pt' % (path, name, name, num))
+        print("%s dataset, part %d, is saved." % (name, num))
+        print("------------------------------")
+    
+    def reset(self):
+        self.array = np.zeros(shape=self.shape)
+
+
+
+
 
 def dataset_build(train_val_ratio=0.1):
     path_project = str(((Path(__file__).parent).resolve().parent).resolve())
@@ -26,9 +55,9 @@ def dataset_build(train_val_ratio=0.1):
     train_count, val_count = 0, 0
     train_num, val_num, = 1, 1
 
-    val = np.zeros(shape=(1000, 12, 273, 256))
+    val = numpy_array((1000, 12, 273, 256))
     val_label = []
-    train = np.zeros(shape=(1000, 12, 273, 256))
+    train = numpy_array((1000, 12, 273, 256))
     train_label = []
 
     for dir in os.listdir(path_data+'/train/'):
@@ -37,84 +66,68 @@ def dataset_build(train_val_ratio=0.1):
 
             if file_name in data_val:
                 if val_count < 1000:
-                    np_array = np.load(path_data+'/train/'+dir+'/'+file)
-                    val[val_count, : 6, :, :] = np_array
-                    np_array = np.transpose(np_array, (0, 2, 1))
-                    mel = np.array([librosa.feature.melspectrogram(S=x, sr=192000, n_mels=256, fmax=96000) for x in np_array])
-                    mel = np.transpose(mel, (0, 2, 1))
-                    val[val_count, 6:, :, :] = mel
-                    val_label.append(list(data[data.id==file_name]['target'])[0])
-                    if val_count == 999 and val_num == 6:
-                        val = torch.from_numpy(val).float()
-                        val_label = torch.from_numpy(np.array(val_label))
-                        val_dst = TensorDataset(val, val_label)
-                        torch.save(val_dst, '%s/datasets_mel/val/val_%d.pt' % (path_data, 6))
-                        print(val_num)
 
+                    np_array = np.load(path_data+'/train/'+dir+'/'+file)
+                    val.transform(np_array=np_array, count=val_count)
+                    val_label.append(list(data[data.id==file_name]['target'])[0])
+
+                    if val_count == 999 and val_num == 6:
+                        val.save_dataset(
+                            label=val_label, num=val_num,
+                            path=path_data, name='val'
+                        )
 
                     val_count += 1
                      
                 else:
                     val_count = 0
-                    val = torch.from_numpy(val).float()
-                    val_label = torch.from_numpy(np.array(val_label))
-                    val_dst = TensorDataset(val, val_label)
-                    torch.save(val_dst, '%s/datasets_mel/val/val_%d.pt' % (path_data, val_num))
-                    print(val_num)
+                    val.save_dataset(
+                        label=val_label, num=val_num,
+                        path=path_data, name='val'
+                    )
 
                     val_num += 1
 
-                    val = np.zeros(shape=(1000, 12, 273, 256))
+                    val.reset()
                     val_label = []
 
                     np_array = np.load(path_data+'/train/'+dir+'/'+file)
-                    val[val_count, : 6, :, :] = np_array
-                    np_array = np.transpose(np_array, (0, 2, 1))
-                    mel = np.array([librosa.feature.melspectrogram(S=x, sr=192000, n_mels=256, fmax=96000) for x in np_array])
-                    mel = np.transpose(mel, (0, 2, 1))
-                    val[val_count, 6:, :, :] = mel
+                    val.transform(np_array=np_array, count=val_count)
                     val_label.append(list(data[data.id==file_name]['target'])[0])
+
                     val_count += 1
             
             else:
                 if train_count < 1000:
+
                     np_array = np.load(path_data+'/train/'+dir+'/'+file)
-                    train[train_count, : 6, :, :] = np_array
-                    np_array = np.transpose(np_array, (0, 2, 1))
-                    mel = np.array([librosa.feature.melspectrogram(S=x, sr=192000, n_mels=256, fmax=96000) for x in np_array])
-                    mel = np.transpose(mel, (0, 2, 1))
-                    train[train_count, 6:, :, :] = mel
+                    train.transform(np_array=np_array, count=train_count)
                     train_label.append(list(data[data.id==file_name]['target'])[0])
 
                     if train_count == 999 and train_num == 54:
-                        train = torch.from_numpy(train).float()
-                        train_label = torch.from_numpy(np.array(train_label))
-                        train_dst = TensorDataset(train, train_label)
-                        torch.save(train_dst, '%s/datasets_mel/train/train_%d.pt' % (path_data, 54))
-                        print(train_num)
+                        train.save_dataset(
+                            label=train_label, num=train_num,
+                            path=path_data, name='train'
+                        )
 
                     train_count += 1
+
                 else:
                     train_count = 0
-                    print(train.shape)
-                    train = torch.from_numpy(train).float()
-                    train_label = torch.from_numpy(np.array(train_label))
-                    train_dst = TensorDataset(train, train_label)
-                    torch.save(train_dst, '%s/datasets_mel/train/train_%d.pt' % (path_data, train_num))
-                    print(train_num)
+                    train.save_dataset(
+                        label=train_label, num=train_num,
+                        path=path_data, name='train'
+                    )
 
                     train_num += 1
 
-                    train = np.zeros(shape=(1000, 12, 273, 256))
+                    train.reset()
                     train_label = []
                     
                     np_array = np.load(path_data+'/train/'+dir+'/'+file)
-                    train[train_count, : 6, :, :] = np_array
-                    np_array = np.transpose(np_array, (0, 2, 1))
-                    mel = np.array([librosa.feature.melspectrogram(S=x, sr=192000, n_mels=256, fmax=96000) for x in np_array])
-                    mel = np.transpose(mel, (0, 2, 1))
-                    train[train_count, 6:, :, :] = mel
+                    train.transform(np_array=np_array, count=train_count)
                     train_label.append(list(data[data.id==file_name]['target'])[0])
+
                     train_count += 1
 
 
